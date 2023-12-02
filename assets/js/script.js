1fa5d5cb1c58b66221623b85da1c9301c5551926
m-levy0 <132528565+m-levy0@users.noreply.github.com>
2023-12-02 23:37:01 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,76 +12,67 @@ postCategories.set('category-hyperlink-post', 'make-bold');
 
 /**
  * Toggles the selected category on click and removes any effect on other categories
- * 
- * @param {The category selected by clicking} selectedCategory 
- * @param {This is map. List of categories available} listOfCategories 
+ *
+ * @param {The category selected by clicking} selectedCategory
+ * @param {This is map. List of categories available} listOfCategories
  */
 function showSelectedCategoryOnly(selectedCategory, listOfCategories) {
-  // Toggle the selected category class
-  document.getElementsByClassName(selectedCategory)['0'].classList.toggle(listOfCategories.get(selectedCategory));
-
-  // remove selection classes from all other categories
-  for (let [key, value] of listOfCategories) {
-
-    // skip the selected category as we don't want it to be manipulated
-    if (key === selectedCategory) {
-      continue;
-    }
-    let re = new RegExp(value, "g");
-    document.getElementsByClassName(key)['0'].className = document.getElementsByClassName(key)['0'].className.replace(re, "");
-  }
-
+	for (let [key, value] of listOfCategories) {
+		if (key === selectedCategory) {
+			document.getElementsByClassName(key)['0'].classList.add(value);
+		} else {
+			document.getElementsByClassName(key)['0'].classList.remove(value);
+		}
+	}
 }
 
 /**
  * Remove the classes from non-selected category posts
- * @param {The category selected by clicking} selectedCategory 
- * @param {This is map. List of categories available} listOfCategories 
+ * @param {The category selected by clicking} selectedCategory
+ * @param {This is map. List of categories available} listOfCategories
  */
 function removeSelectionFromOtherCategoryPosts(selectedCategory, listOfCategories) {
-
-  for (let [key, value] of listOfCategories) {
-    // skip the selected category as we don't want it to be manipulated
-    if (key === selectedCategory) {
-      continue;
-    }
-    // get list of posts to be unselected
-    let unselectedElementsList = document.getElementsByClassName(key);
-    let loopIndex;
-    let re = new RegExp(value, "g");
-    for (loopIndex = 0; loopIndex < unselectedElementsList.length; loopIndex++) {
-      unselectedElementsList[loopIndex].className = unselectedElementsList[loopIndex].className.replace(re, "");
-    }
-  }
-
+	for (let [key, value] of listOfCategories) {
+		if (key !== selectedCategory) {
+			let unselectedElementsList = document.getElementsByClassName(key);
+			for (let i = 0; i < unselectedElementsList.length; i++) {
+				unselectedElementsList[i].style.display = 'none';
+			}
+		} else {
+			let selectedElementsList = document.getElementsByClassName(key);
+			for (let i = 0; i < selectedElementsList.length; i++) {
+				selectedElementsList[i].style.display = 'block';
+			}
+		}
+	}
 }
 
 /**
  * Adds given class to all elements of target class
- * @param { The class to find elements for } targetElement 
- * @param { The class to add } classToAdd 
+ * @param { The class to find elements for } targetElement
+ * @param { The class to add } classToAdd
  */
 function addClassToAll(targetElement, classToAdd) {
-  var catElements = document.getElementsByClassName(targetElement);
-  var catIndex;
-  for (catIndex = 0; catIndex < catElements.length; catIndex++) {
-    catElements[catIndex].classList.toggle(classToAdd);
-  }
+	var catElements = document.getElementsByClassName(targetElement);
+	var catIndex;
+	for (catIndex = 0; catIndex < catElements.length; catIndex++) {
+		catElements[catIndex].classList.toggle(classToAdd);
+	}
 }
 
 // The function calls from HTML
 function showCategoryImage() {
-  showSelectedCategoryOnly('category-image-link', categories);
-  addClassToAll('category-image-post', 'make-bold');
-  removeSelectionFromOtherCategoryPosts('category-image-post', postCategories);
+	showSelectedCategoryOnly('category-image-link', categories);
+	addClassToAll('category-image-post', 'make-bold');
+	removeSelectionFromOtherCategoryPosts('category-image-post', postCategories);
 }
 function showCategoryText() {
-  showSelectedCategoryOnly('category-text-link', categories);
-  addClassToAll('category-text-post', 'make-bold');
-  removeSelectionFromOtherCategoryPosts('category-text-post', postCategories);
+	showSelectedCategoryOnly('category-text-link', categories);
+	addClassToAll('category-text-post', 'make-bold');
+	removeSelectionFromOtherCategoryPosts('category-text-post', postCategories);
 }
 function showCategoryHyperlink() {
-  showSelectedCategoryOnly('category-hyperlink-link', categories);
-  addClassToAll('category-hyperlink-post', 'make-bold');
-  removeSelectionFromOtherCategoryPosts('category-hyperlink-post', postCategories);
+	showSelectedCategoryOnly('category-hyperlink-link', categories);
+	addClassToAll('category-hyperlink-post', 'make-bold');
+	removeSelectionFromOtherCategoryPosts('category-hyperlink-post', postCategories);
 }
